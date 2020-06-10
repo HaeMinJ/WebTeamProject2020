7ce4fdefb3c572d916aaf8437944550d745c2222
yexjin <49095587+yexjin@users.noreply.github.com>
2020-06-11 00:36:33 +0900

    방문예약 footer+예약확인화면
    끝!

--- js/reservation.js
+++ js/reservation.js
@@ -10,12 +10,18 @@ function calc() {
 
 function confirmOrder() {
     calc()
-    confirm("예약일: "+document.getElementById("date").value+", 방문시간: "+document.getElementById("time").value+
+    var user = confirm("예약일: "+document.getElementById("date").value+", 방문시간: "+document.getElementById("time").value+
     ", 총 " +document.getElementById("people").value + "명, "+
     document.getElementById("run").value+"시간 게임하실 것으로 확인 되셨습니다. 가격은 "+
     calc()+"입니다. 예약하시겠습니까?")
+
+    if(!user)
+    {
+        location.href="./reservation.html"
+    }
+    else
+    {
+        location.href="./reservation_clear.html"
+    }
 }
 
-function change() {
-    location.href="./reservation_clear.html"
-}
