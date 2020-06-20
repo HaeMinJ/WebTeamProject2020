26e016e1861a8c1935660e015dc6721199ff186f
haemin <goals001214@gmail.com>
2020-06-20 14:16:12 +0900

    -전체 폴더구조 수정 -개인정보 처리방침 수정 -확장자 대문자로 되어있는거 전부 소문자로 수정

--- js/reservation.js
+++ js/reservation.js
@@ -1,27 +1,24 @@
 function calc() {
-    var p=document.getElementById("people").value;
-    var r=document.getElementById("run").value;
+    var p = document.getElementById("people").value;
+    var r = document.getElementById("run").value;
 
-    var price=parseInt(p)*parseInt(r)*2000;
+    var price = parseInt(p) * parseInt(r) * 2000;
 
     return price;
 }
 
 
 function confirmOrder() {
-    calc()
-    var user = confirm("예약일: "+document.getElementById("date").value+", 방문시간: "+document.getElementById("time").value+
-    ", 총 " +document.getElementById("people").value + "명, "+
-    document.getElementById("run").value+"시간 게임하실 것으로 확인 되셨습니다. 가격은 "+
-    calc()+"입니다. 예약하시겠습니까?")
+    calc();
+    var user = confirm("예약일: " + document.getElementById("date").value + ", 방문시간: " + document.getElementById("time").value +
+        ", 총 " + document.getElementById("people").value + "명, " +
+        document.getElementById("run").value + "시간 게임하실 것으로 확인 되셨습니다. 가격은 " +
+        calc() + "입니다. 예약하시겠습니까?");
 
-    if(!user)
-    {
-        location.href="./reservation.html"
-    }
-    else
-    {
-        location.href="./reservation_clear.html"
+    if (!user) {
+        location.href = "./reservation.html"
+    } else {
+        location.href = "./reservation_clear.html"
     }
 }
 
